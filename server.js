73b4e587a8e6156186cd0cb3605f4a4954b2b2a6
Pixls112 <nguyen.phillip112@gmai.com>
2023-08-28 22:59:13 -0700

    added app.get methods for requirement

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const app = express()
 const PORT = process.env.PORT || 3001;
 const index = './Develop/public/index.html'
 const note = './Develop/public/notes.html'
+const fs = require('fs')
+const noteData = require('./Develop/db/db.json')
+
+
 
 app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, index)))
+res.sendFile(path.join(__dirname, index)));
 
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, note)))
+res.sendFile(path.join(__dirname, note)));
+
+app.get('*', (req, res) => 
+res.sendFile(path.join(__dirname, index)));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(noteData, (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            return res.json(data)
+        }
+    })
+})
 
 app.listen(PORT, () =>
-    console.log(`App listening on ${PORT}`))
+    console.log(`App listening on http://localHost:${PORT}`))
